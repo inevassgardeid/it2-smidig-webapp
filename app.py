63416e1757cbd2984edbb8ef4f7f63f807f73d6d
inevassgardeid <123069085+inevassgardeid@users.noreply.github.com>
2023-02-16 11:00:26 +0100

    oppdatert app.py og html

--- app.py
+++ app.py
@@ -7,4 +7,14 @@ def index():
     navn = "Hjem"
     return render_template("index.html", navn=navn)
 
+@app.route("/onsker")
+def index():
+    navn = "Ønsker å lese"
+    return render_template("onsker.html", navn=navn)
+
+@app.route("/lest")
+def index():
+    navn = "Leste bøker"
+    return render_template("lest.html", navn=navn)
+
 app.run(debug=True)
