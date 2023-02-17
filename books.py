eb9d8313f274af12013e885a9612f468dd2a883e
inevassgardeid <123069085+inevassgardeid@users.noreply.github.com>
2023-02-17 09:47:26 +0100

    ordnet i småfeil og css

--- books.py
+++ books.py
@@ -1,11 +1,12 @@
 import json
+
 def hent_books():
     fil = open("books.json")
     bøker = json.load(fil)
     fil.close()
-    for bok in bøker:
-        bokliste = []
-        bilde = bok["imageLink"]
-        bokliste.append(bilde)
-        return bokliste
-    
+    # for bok in bøker:
+    #     bokliste = []
+    #     bilde = bok["imageLink"]
+    #     bokliste.append(bilde)
+    #     return bokliste
+    return bøker
